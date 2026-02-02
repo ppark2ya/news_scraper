4a22fd6067e1000bf6f2fb429d34cb69d24e2c1c
ppark2ya <vigabhan1231@gmail.com>
2026-02-02 22:20:14 +0900

    뉴욕타임즈 스크래핑 기본 코드 작성

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+use reqwest;
+use scraper::{Html, Selector};
+
 fn main() {
+    // 1. HTTP 요청
+    let body = reqwest::blocking::get("https://www.nytimes.com")
+        .unwrap()
+        .text()
+        .unwrap();
+
+    // 2. 파싱
+    let document = Html::parse_document(&body);
+    let selector = Selector::parse("h2").unwrap();
+
+    for element in document.select(&selector) {
+        println!("{}", element.text().collect::<Vec<_>>().join(""));
+    }
 }
